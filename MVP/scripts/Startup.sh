7abaa9133bfa2526c564818d7fb2b6b32c6e6f48
webbhm <webb.howard@gmail.com>
2019-03-19 10:35:49 -0400

    Add files via upload

--- MVP/scripts/Startup.sh
+++ MVP/scripts/Startup.sh
@@ -7,23 +7,19 @@
 #Author: Howard Webb
 #Date: 7/15/2017
 
-echo Running Startup
+echo \nRunning Startup >> /home/pi/MVP/logs/startup.log 2>&1
 
 sudo chmod 666 /home/pi/MVP/logs/*
 
-echo Starting CouchDB
+echo Starting CouchDB >> /home/pi/MVP/logs/startup.log 2>&1
 # Start CouchDB
 /home/pi/MVP/scripts/StartCouchDB.sh >> /home/pi/MVP/logs/startup.log 2>&1
 
-echo Starting Server
+echo Starting Server >> /home/pi/MVP/logs/startup.log 2>&1
 # Start Server
 /home/pi/MVP/scripts/StartServer.sh >> /home/pi/MVP/logs/startup.log 2>&1
 
-sudo chmod 666 /home/pi/MVP/logs/*
-
-# add sleep to see if this will give time for the couchdb library stuff to get set up.
-sleep 10
-echo Check Lights, etc
+echo Check Lights, etc >> /home/pi/MVP/logs/startup.log 2>&1
 # Run startup code
 python3 /home/pi/MVP/python/StartUp.py >> /home/pi/MVP/logs/startup.log 2>&1
 
