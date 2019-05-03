b4757cf48dcaa8517d36613fc00b9fa93a259606
webbhm <webb.howard@gmail.com>
2019-05-03 12:21:09 -0400

    Update LogMVP.sh

--- MVP/scripts/LogMVP.sh
+++ MVP/scripts/LogMVP.sh
@@ -7,6 +7,7 @@ echo $(date +"%D %T") "Log Sensors"
 #Log std JSON data
 python3 /home/pi/MVP/python/LogSensors.py
 
-#Log std JSON data
-python3 /home/pi/MVP/python/LogSensorsExtra.py
+#Log optional sensors
+# Uncomment this (and the particular sensors) to log these options
+#python3 /home/pi/MVP/python/LogSensorsExtra.py
 
