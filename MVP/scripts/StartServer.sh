32f827308e57bfe162a0f86541cb17739ab029e3
webbhm <webb.howard@gmail.com>
2019-05-15 20:02:51 -0400

    Add files via upload
    Name Change, update for logging

--- MVP/scripts/StartServer.sh
+++ MVP/scripts/StartServer.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 #Script to start up the web server
+#This should be placed in a startup directory so it runs every time the Pi is booted
+#There are several ways to do this, but the following is one
+#https://www.raspberrypi.org/documentation/linux/usage/rc-local.md
 #Author: Howard Webb
 #Date: 7/15/2017
 
 #NOTE: The server must be started from the directory from which files are to be served
 cd /home/pi/MVP/web
-
-echo
-echo  $(date +"%D %T") Starting Server >> /home/pi/MVP/logs/server.log
-
-# run in background so other processes in the calling script get run
-python3 -m http.server 2>&1 | tee -a /home/pi/MVP/logs/server.log &
+python /home/pi/MVP/python/WebServer.py &> /home/pi/MVP/logs/server.log &
