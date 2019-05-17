5347e730646882dad4456178c65f629e27f1e8a4
webbhm <webb.howard@gmail.com>
2019-05-17 16:04:00 -0400

    Add files via upload
    replace nohup in start call

--- MVP/scripts/StartCouchDB.sh
+++ MVP/scripts/StartCouchDB.sh
@@ -6,5 +6,5 @@
 
 # only this line should call this log due to ownership by sudo
 # use & at end of file so runs in background
-sudo -i -u couchdb /home/couchdb/bin/couchdb 2>&1 | tee -a /home/pi/MVP/logs/couchdb.log &
+nohup sudo -i -u couchdb /home/couchdb/bin/couchdb 2>&1 | tee -a /home/pi/MVP/logs/couchdb.log &
 
