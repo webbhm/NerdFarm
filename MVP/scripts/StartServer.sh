c5d8c8a25451d3d547da602ab12687c2d6f6d800
webbhm <webb.howard@gmail.com>
2019-05-17 13:35:36 -0400

    Add files via upload
    Revert to single line (no logging) and run in background

--- MVP/scripts/StartServer.sh
+++ MVP/scripts/StartServer.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
 #Script to start up the web server
+#This should be placed in a startup directory so it runs every time the Pi is booted
+#There are several ways to do this, but the following is one
+#https://www.raspberrypi.org/documentation/linux/usage/rc-local.md
 #Author: Howard Webb
 #Date: 7/15/2017
 
 #NOTE: The server must be started from the directory from which files are to be served
-
 cd /home/pi/MVP/web
-
-# Note this simple server is the Python3 upgrade.  It works nicely, but doesn't log data
-# If you are interested in more logging, you can dig in here:
-# https://medium.com/@andrewklatzke/creating-a-python3-webserver-from-the-ground-up-4ff8933ecb96
-
-python3 -m http.server &> /home/pi/MVP/logs/server.log &
+python3 -m http.server &
